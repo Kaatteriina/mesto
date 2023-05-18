43991d97000891397db3d853882d6fd488e89065
Korolkova Ekaterina <k_katu6k@mail.ru>
2023-05-18 14:45:44 +0500

    commit 2.0

--- script/index.js
+++ script/index.js
@@ -5,21 +5,31 @@ const form = document.querySelector(".popup__form");
 const nameInput = document.getElementById("name");
 const aboutInput = document.getElementById("about");
 const saveButton = document.querySelector(".popup__save-button");
+//const profileName = document.querySelector(".profile__name");
+//const profileDescription = document.querySelector(".profile__description");
+
 
 editButton.addEventListener("click", function () {
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
+  //nameInput.value = currentUserData.name;
+//aboutInput.value = currentUserData.about;
 });
 
 closeButton.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 });
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
   document.querySelector(".profile__name").textContent = nameInput.value;
+
   console.log("save button");
+
   document.querySelector(".profile__description").textContent =
     aboutInput.value;
-  popup.classList.remove("popup__opened");
+
+  popup.classList.remove("popup_opened");
 });
-     
+
+
